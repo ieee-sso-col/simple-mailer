ef517f934703bef7b489d0e022d3acbe29b9dae5
Johan Tique <johan.tique@analisistem.com>
2016-04-04 23:15:20 -0500

    adapt to ieee

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'csv'
-
-CSV.foreach(File.join(Rails.root, 'people.csv'), headers: true) do |row|
-  Person.find_or_create_by name: row['Name'].downcase, email: row['Email'].downcase, gender: row['Gender']
-end
-
-Person.find_or_create_by name: 'Johan Tique', email: 'johan.tique@gmail.com', gender: 'm', rol: 'assistant'
-Person.find_or_create_by name: 'Johan Tique', email: 'johan.tique@analisistem.com', gender: 'm', rol: 'admin'
-Person.find_or_create_by name: 'Johan Tique', email: 'johan_tique@yahoo.com.co', gender: 'm', rol: 'admin'
-Person.find_or_create_by name: 'Johan Tique', email: 'johan_tique@hotmail.com', gender: 'm', rol: 'admin'
-Person.find_or_create_by name: 'Felipe Muñoz', email: 'pipebermeo14@gmail.com', gender: 'm', rol: 'admin'
+Person.find_or_create_by name: 'Johan Tique', email: 'johan.tique@gmail.com', gender: 'm', rol: 'admin'
